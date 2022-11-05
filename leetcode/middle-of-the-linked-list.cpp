bb9a76c4e14e5b2b0e05c231e8d5010bd84a2695
Abel Mulugeta <abelmulugeta2014@gmail.com>
2022-11-05 13:38:42 +0300

    middle-of-the-linked-list
    https://leetcode.com/problems/middle-of-the-linked-list/submissions/
    
    5ms 7.2mb

---
+++ leetcode/middle-of-the-linked-list.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        int size = 0, half_size = 0;
+        ListNode* ptr = head;
+
+        while(ptr != nullptr) {
+            ++size;
+            ptr = ptr->next;
+        }
+        
+        half_size = size / 2;
+
+        ptr = head;
+        
+        while(half_size != 0) {
+            ptr = ptr->next;
+            --half_size;
+        }
+        
+        return ptr;
+    }
+};
