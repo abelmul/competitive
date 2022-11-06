697a34175d85f7a879092c0093c6ba8a3986cb21
Abel Mulugeta <abelmulugeta2014@gmail.com>
2022-11-06 18:23:09 +0300

    reverse-substrings-between-each-pair-of-parentheses
    https://leetcode.com/problems/reverse-substrings-between-each-pair-of-parentheses/submissions/
    
    0ms 6.4mb

---
+++ leetcode/reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    char topandpop(vector<char>& t) {
+      auto s = t.back();
+      t.pop_back();
+      return s;
+    }
+
+    string reverseParentheses(string str) {
+        vector<char> reverser;
+        
+        for(char s:str) {
+            if (s == ')') {
+                char i = topandpop(reverser);
+                string reversed = "";
+                while(i != '(') {
+                    reversed.push_back(i);
+                    i = topandpop(reverser);
+                }
+                
+                for(char r: reversed) {
+                    reverser.push_back(r);
+                }
+            } else {
+                reverser.push_back(s);
+            }
+        }
+
+        return string(reverser.begin(), reverser.end());
+    }
+};
