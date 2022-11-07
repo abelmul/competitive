982083e96dd3579f76503a0b257c27e29d58be30
Abel Mulugeta <abelmulugeta2014@gmail.com>
2022-11-07 20:05:58 +0300

    xor-queries-of-a-subarray
    https://leetcode.com/problems/xor-queries-of-a-subarray
    
    164ms 37.7mb

---
+++ leetcode/xor-queries-of-a-subarray.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        auto size = arr.size();
+        auto queries_size = queries.size();
+        vector<int> prefix_sum = vector<int>(size + 1);
+        vector<int> answer = vector<int>(queries.size());
+        
+        for(int i = 1; i < size + 1; ++i) {
+            prefix_sum[i] = prefix_sum[i-1] ^ arr[i-1];
+        }
+        
+        for(int i = 0; i < queries_size; ++i) {
+            answer[i] = prefix_sum[queries[i][0]] ^ prefix_sum[queries[i][1]+1];
+        }
+        
+        return answer;
+    }
+};
