1cc1d90766641de34dfaefd7eba3e188e61ffd2e
Abel Mulugeta <abelmulugeta2014@gmail.com>
2022-11-07 21:34:57 +0300

    subarray-sum-equals-k
    https://leetcode.com/problems/subarray-sum-equals-k/
    
    177ms 41.4mb

---
+++ leetcode/subarray-sum-equals-k.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int size = nums.size();
+        unordered_map<int,int> prev_sum;
+        int current_sum = 0;
+        int count = 0;
+        
+        for(int i = 0; i < size; ++i) {
+            current_sum += nums[i];
+            
+            if (current_sum == k)
+                ++count;
+            if (prev_sum.find(current_sum-k) != prev_sum.end())
+                count += prev_sum[current_sum - k];
+            
+            prev_sum[current_sum]++;
+        }
+        return count;
+    }
+};
