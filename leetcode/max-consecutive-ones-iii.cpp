d248c49f4c88a2c6ca2db9af91ac89a4f1626299
Abel Mulugeta <abelmulugeta2014@gmail.com>
2022-11-07 22:19:34 +0300

    max-consecutive-ones-iii
    https://leetcode.com/problems/max-consecutive-ones-iii/submissions/
    
    139ms 58.4mb

---
+++ leetcode/max-consecutive-ones-iii.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int i = 0;
+        int current_max = 0;
+        auto size = nums.size();
+        size_t zeros_size;
+        vector<int> zeros = {-1};
+        
+        for(int i = 0; i < size; ++i) {
+            if (nums[i] == 0)
+                zeros.push_back(i);
+        }
+        zeros.push_back(size);
+        
+        zeros_size = zeros.size();
+        
+        if (k > zeros_size - 2)
+            return size;
+        
+        int start = 0;
+        int end = k+1;
+        
+        while(end < zeros_size) {
+            current_max = max(current_max, zeros[end]-zeros[start]-1);
+            ++end;
+            ++start;
+        }
+        
+        return current_max;
+    }
+};
