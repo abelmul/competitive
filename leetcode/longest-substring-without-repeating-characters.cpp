e7dcea5c433ec6f018a7713c6dd7444c48aee2fd
Abel Mulugeta <abelmulugeta2014@gmail.com>
2022-11-07 23:11:56 +0300

    longest-substring-without-repeating-characters
    https://leetcode.com/problems/longest-substring-without-repeating-characters
    
    2023 ms 244mb

---
+++ leetcode/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        auto size = s.size();
+        int current_max = 0;
+        
+        for(int i = 0; i < size; ++i) {
+            int window_max = 1;
+            unordered_map<char,bool> map;
+            
+            map[s[i]] = true;
+            
+            for(int j = i+1; j < size; ++j) {
+                if (map.find(s[j]) != map.end())
+                    break;
+
+                map[s[j]] = true;
+                ++window_max;
+            }
+            current_max = max(window_max, current_max);
+        }
+        
+        return current_max;
+    }
+};
