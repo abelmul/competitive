fe6e9bad7b0fa83a0aed6be87408acb8e8b9f39c
Abel Mulugeta <abelmulugeta2014@gmail.com>
2022-11-10 12:27:56 +0300

    k-closest-points-to-origin
    https://leetcode.com/problems/k-closest-points-to-origin/
    
    2588ms 234.4mb

---
+++ leetcode/k-closest-points-to-origin.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        sort(points.begin(), points.end(), [](vector<int> a, vector<int> b) {
+           return sqrt(pow(a[0], 2)+pow(a[1],2)) < sqrt(pow(b[0], 2)+pow(b[1],2));
+        });
+        
+        return vector<vector<int>>(points.begin(), points.begin()+k);
+    }
+};
