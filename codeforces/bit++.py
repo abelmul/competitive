0dde390affad420390450640ecd96c0db9c9ba0c
Abel Mulugeta <abelmulugeta2014@gmail.com>
2022-12-26 20:37:52 +0300

    bit++
    https://codeforces.com/problemset/problem/282/A

---
+++ codeforces/bit++.py
@@ -0,0 +1,13 @@
+x = 0
+
+commands = int(input())
+
+for i in range(commands):
+    command = input()
+
+    if '+' in [command[0], command[-1]]:
+        x += 1
+    elif '-' in [command[0], command[-1]]:
+        x -= 1
+
+print(x)
