e6a24986532d41bdff95149eaf534655b95ebdba
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-01-04 20:25:13 +0300

    verifying-an-alien-dictionary
    https://leetcode.com/problems/verifying-an-alien-dictionary/
    
    39ms 13.9mb

---
+++ leetcode/verifying-an-alien-dictionary.py
@@ -0,0 +1,33 @@
+from functools import cmp_to_key, partial
+
+class Solution:
+    def compare_by(order, str1, str2):
+        if str1 == str2:
+            return 0
+        
+        # calculate length of the strings
+        length1 = len(str1)
+        length2 = len(str2)
+        length = min(length1, length2)
+
+        for i in range(length):
+            index1 = order.index(str1[i])
+            index2 = order.index(str2[i])
+
+            if index1 > index2:
+                return 1
+            elif index1 < index2:
+                return -1
+        
+        if length == length1:
+            return -1
+        else:
+            return 1
+
+
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        return words == sorted(
+            words, 
+            key=cmp_to_key(partial(Solution.compare_by, order))
+        )
+
