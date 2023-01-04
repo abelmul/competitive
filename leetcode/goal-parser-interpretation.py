8ff49385af891b168f5881ba5666a4644fb671a9
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-01-04 21:31:25 +0300

    goal-parser-interpretation
    https://leetcode.com/problems/goal-parser-interpretation/
    
    48ms 13.8ms

---
+++ leetcode/goal-parser-interpretation.py
@@ -0,0 +1,28 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        result = ""
+
+        li = []
+        in_parens = False
+
+        for c in command:
+            if c == '(':
+                in_parens = True
+                li.append(c)
+            elif c == ')':
+                in_parens = False
+
+                
+                if li[-1] == '(':
+                    result += 'o'
+                else:
+                    result += "".join(li[1:])
+
+                li = []
+            else:
+                if in_parens:
+                    li.append(c)
+                else:
+                    result += c
+
+        return result
