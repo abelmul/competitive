befb95363ac28de8e27e0cf89c2e2f465dac0bc1
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-01-04 21:05:01 +0300

    decrypt-string-from-alphabet-to-integer-mapping
    https://leetcode.com/problems/decrypt-string-from-alphabet-to-integer-mapping
    
    67ms 13.9mb

---
+++ leetcode/decrypt-string-from-alphabet-to-integer-mapping.py
@@ -0,0 +1,16 @@
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        length = len(s)
+        i = 0
+
+        decrypted = ""
+
+        while i < length:
+            if i+2 < length and s[i+2] == '#':
+                decrypted += chr(ord('a') + int(s[i]+s[i+1]) - 1)
+                i += 3
+            else:
+                decrypted += chr(ord('a') + int(s[i]) - 1)
+                i += 1
+
+        return decrypted
