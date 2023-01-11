5bd6bbdae51f44472b65e280e3582f034e4ba2cc
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-01-11 21:09:54 +0300

    missing-number
    https://leetcode.com/problems/missing-number
    
    133ms 15.2mb

---
+++ leetcode/missing-number.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        length = len(nums)
+        res = length
+
+        if nums[0] != 0:
+            res = 0
+        else:
+            for i in range(1, length):
+                if nums[i] != nums[i-1] + 1:
+                    res = nums[i-1] + 1
+                    break
+
+        return res
