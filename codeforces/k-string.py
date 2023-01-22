6067673135bfb4b953741b738a8484c5ccd08cf3
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-01-22 13:27:26 +0300

    k-string
    https://codeforces.com/contest/219/problem/A
    
    184ms 0kb

---
+++ codeforces/k-string.py
@@ -0,0 +1,16 @@
+k = int(input())
+s = input()
+
+char_count = {}
+for c in s:
+    char_count[c] = char_count.get(c, 0) + 1
+
+segment = []
+res = ""
+for key,value in char_count.items():
+    if value % k != 0:
+        res = "-1"
+        break
+    segment.append(key*(value//k))
+
+print("-1" if res == "-1" else "".join(segment * k))
