81b81865d050cc471903c6d8cf0e90507fe74b9e
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-01-22 13:35:03 +0300

    transpose-matrix
    https://leetcode.com/problems/transpose-matrix/
    
    73ms 14.8mb

---
+++ leetcode/transpose-matrix.py
@@ -0,0 +1,10 @@
+from typing  import List
+
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        row = len(matrix)
+        column = len(matrix[0])
+
+        transpose = [[matrix[j][i] for j in range(row)] for i in range(column)]
+
+        return transpose
