c668cca37e13364436b64b7647cdf529bbb7bf03
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-01-22 11:55:31 +0300

    count-good-meals
    https://leetcode.com/problems/count-good-meals/
    
    1253ms 21.5mb

---
+++ leetcode/count-good-meals.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def countPairs(self, deliciousness: List[int]) -> int:
+        power_of_two = [2**i for i in range(22)]
+        deliciousness_dict = {}
+        
+        ans = 0
+        for d in deliciousness:
+            for power in power_of_two:
+                if power - d in deliciousness_dict:
+                    ans += deliciousness_dict[power-d]
+            deliciousness_dict[d] = deliciousness_dict.get(d, 0) + 1
+
+        return ans % (10**9 + 7)
