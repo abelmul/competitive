052cd7e254cca5e2149c6a91eec191a8bdbe208c
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-01-23 19:22:44 +0300

    contests: week 2
    https://codeforces.com/gym/420178

---
+++ contests/week2/3-dakti.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+n = int(input())
+
+for i in range(n):
+    sentense = input().split()
+
+    chorus: Any = [None] * len(sentense)
+
+    for s in sentense:
+        letter = []
+        digit = []
+        for j, c in enumerate(s):
+            if c.isdigit():
+                digit.append(c)
+            else:
+                letter.append(c)
+        chorus[int("".join(digit)) - 1] = "".join(letter)
+
+    print(" ".join(chorus))
