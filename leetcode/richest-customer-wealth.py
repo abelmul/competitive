8025e2d6424ef7c428791719635a133c7a1efbf0
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-01-24 17:09:12 +0300

    richest-customer-wealth
    https://leetcode.com/problems/richest-customer-wealth/
    
    54ms 13.8mb

---
+++ leetcode/richest-customer-wealth.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        max_sum = 0
+        n = len(accounts)
+
+        for i in range(n):
+            max_sum = max(max_sum, sum(accounts[i]))
+        
+        return max_sum
