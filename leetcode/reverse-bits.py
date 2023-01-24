294bf5ecdf0a601eec3d559a316d2ddaf7be439b
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-01-24 17:42:42 +0300

    reverse-bits
    https://leetcode.com/problems/reverse-bits/
    
    29ms 13.9mb

---
+++ leetcode/reverse-bits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+
+        reversed = 0
+
+        for _ in range(32):
+            reversed = ((reversed << 1) | (n&1))
+            n = n >> 1
+
+        return reversed
