231d5a2d314c19c15310846edccf34f4dda4911d
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-01-24 17:00:18 +0300

    running-sum-of-1d-array
    https://leetcode.com/problems/running-sum-of-1d-array/
    
    45ms 13.9mb

---
+++ leetcode/running-sum-of-1d-array.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        length = len(nums)
+
+        for i in range(1, length):
+            nums[i] += nums[i-1]
+
+        return nums
