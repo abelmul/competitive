24e7f6607cbe8f1f663b234438c114d99862a61b
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-02-09 11:51:58 +0300

    remove-element
    32ms 13.9mb

---
+++ leetcode/remove-element.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            if nums[r] == val:
+                r -= 1
+            elif nums[l] == val:
+                nums[l] = nums[r]
+
+                l += 1
+                r -= 1
+            else:
+                l += 1
+
+        return r+1
