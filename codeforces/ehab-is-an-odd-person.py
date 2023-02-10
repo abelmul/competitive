9c571c1e9bded00916f64990ad802ef491be2de7
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-02-10 10:35:55 +0300

    ehab-is-an-odd-person
    https://codeforces.com/problemset/problem/1174/B
    
    327ms 12200kb

---
+++ codeforces/ehab-is-an-odd-person.py
@@ -0,0 +1,18 @@
+n = int(input())
+a = list(map(int, input().split()))
+
+odd = False
+even = False
+
+for x in a:
+    if x % 2 == 0:
+        even = True
+    else:
+        odd = True
+        
+    if even and odd:
+        a.sort()
+        break
+    
+print(*a)
+        
