34fc321850ca114d51268479ae933b5a935e6f90
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-02-17 12:32:44 +0300

    merging-arrays
    https://codeforces.com/edu/course/2/lesson/9/1/practice/contest/307092/problem/A
    
    451ms 14.6mb

---
+++ codeforces/edu/two-pointer/a-merging-arrays.py
@@ -0,0 +1,20 @@
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+ai = 0
+bi = 0
+
+res = []
+
+while ai < n or bi < m:
+    next_element = float('inf')
+
+    if bi == m or (ai < n and  a[ai] < b[bi]):
+        res.append(a[ai])
+        ai += 1
+    else:
+        res.append(b[bi])
+        bi += 1
+
+print(*res)
