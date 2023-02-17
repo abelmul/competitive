e106b0990da674371ff3380563da08710966f42f
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-02-17 14:06:25 +0300

    segment-with-small-sum
    https://codeforces.com/edu/course/2/lesson/9/2/practice/contest/307093/problem/A
    
    202ms 10.6mb

---
+++ codeforces/edu/two-pointer/2/a-segment-with-small-sum.py
@@ -0,0 +1,14 @@
+n, s = map(int, input().split())
+a = list(map(int, input().split()))
+
+max_elems = current_sum = right = 0
+
+for left in range(n):
+    while right < n and current_sum + a[right] <= s:
+        current_sum += a[right]
+        right += 1
+
+    current_sum -= a[left]
+    max_elems = max(max_elems, right - left)
+    
+print(max_elems)
