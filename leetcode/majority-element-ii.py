2197d61e0e3a59c117e2352f70d87383ee5102ee
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-03-01 17:41:38 +0300

    majority-element-ii
    https://leetcode.com/problems/majority-element-ii
    
    123ms 15.4mb

---
+++ leetcode/majority-element-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        length = len(nums)
+        d = {}
+
+        res = []
+
+        for n in nums:
+            d[n] = d.get(n, 0) + 1
+            if d[n] > length / 3:
+                d[n] = -length
+                res.append(n)
+
+        return res
