279c9dd5a22e224473ef2b5899be337ffc3f314b
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-03-01 17:41:40 +0300

    remove-linked-list-elements
    https://leetcode.com/problems/remove-linked-list-elements
    
    71ms 17.9mb

---
+++ leetcode/remove-linked-list-elements.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+
+        prev = dummy
+        ptr = head
+
+        while ptr:
+            if ptr.val == val:
+                ptr = ptr.next
+                prev.next = ptr
+            else:
+                prev = ptr
+                ptr = ptr.next
+
+        return dummy.next
