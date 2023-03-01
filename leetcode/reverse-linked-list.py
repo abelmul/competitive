29ddaf2e6db09d51ca7432fa19b46f862387751f
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-03-01 23:19:13 +0300

    reverse-linked-list
    https://leetcode.com/problems/reverse-linked-list
    
    30ms 15.4mb

---
+++ leetcode/reverse-linked-list.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        next = None
+
+        if head:
+            next = head.next
+            head.next = None
+
+        while next:
+            next_ptr = next.next
+
+            next.next = head
+            head = next
+
+            next = next_ptr
+
+        return head
