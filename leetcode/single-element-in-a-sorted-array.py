5543634a4dab0fc045a0888ead9279154b205963
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-03-01 23:19:17 +0300

    single-element-in-a-sorted-array
    https://leetcode.com/problems/single-element-in-a-sorted-array
    
    171ms 23.7mb

---
+++ leetcode/single-element-in-a-sorted-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums) - 2
+        while right >= left:
+            mid = (right + left) // 2
+
+            # if mid is even duplicate will be the next element
+            # if mid is odd it will be the previous
+            index = mid + 1 if mid % 2 == 0 else mid - 1
+
+            if nums[mid] == nums[index]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return nums[left]
