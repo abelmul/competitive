1d5687f36df2821ba631b7e4b7a79de9e1a2a67e
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-03-01 17:41:42 +0300

    minimum-common-value
    https://leetcode.com/problems/minimum-common-value
    
    472ms 32.3mb

---
+++ leetcode/minimum-common-value.py
@@ -0,0 +1,17 @@
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        ptr1 = 0
+        ptr2 = 0
+
+        length1 = len(nums1)
+        length2 = len(nums2)
+
+        while ptr1 < length1 and ptr2 < length2:
+            if nums1[ptr1] == nums2[ptr2]:
+                return nums1[ptr1]
+            elif nums1[ptr1] > nums2[ptr2]:
+                ptr2 += 1
+            else:
+                ptr1 += 1
+
+        return -1
