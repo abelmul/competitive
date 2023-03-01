08fb1852cc4d56988d14cddeec800592b2bcf1a6
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-03-01 17:41:37 +0300

    rotate-list
    https://leetcode.com/problems/rotate-list
    
    42ms 13.9mb

---
+++ leetcode/rotate-list.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        length = 0
+        ptr = head
+        while ptr:
+            ptr = ptr.next
+            length += 1
+
+        if length == 0 or k % length == 0:
+            return head
+
+        k = k % length
+        dummy = ListNode(0, head)
+
+        slow_ptr = head
+        fast_ptr = head
+
+        while fast_ptr:
+            # this is -1 because we need the previous node
+            if k > -1:
+                fast_ptr = fast_ptr.next
+                k -= 1
+            else:
+                slow_ptr = slow_ptr.next
+                fast_ptr = fast_ptr.next
+
+        dummy.next = slow_ptr.next
+        slow_ptr.next = None
+        ptr = dummy.next
+
+        while ptr.next:
+            ptr = ptr.next
+
+        ptr.next = head
+
+        return dummy.next
