ec228319ef7e99a23a6a9207745bcbc465072f32
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-03-01 23:04:23 +0300

    insertion-sort-list
    https://leetcode.com/problems/insertion-sort-list
    
    1346ms 16.6mb

---
+++ leetcode/insertion-sort-list.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+
+        prev_node = dummy
+        traverser = head
+
+        while traverser:
+            detached_node = traverser
+
+            prev_node.next = traverser.next
+            traverser = prev_node.next
+
+            prev = dummy
+            current = dummy.next
+
+            while current != traverser and current.val < detached_node.val:
+                prev = current
+                current = current.next
+
+            detached_node.next = current
+            prev.next = detached_node
+
+            if prev_node == prev:
+                prev_node = detached_node
+
+        return dummy.next
