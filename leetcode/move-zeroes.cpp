5d4dd4594eb5273c1fa65de8118cb748e9493e22
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-03-02 10:54:43 +0300

    move-zeroes
    https://leetcode.com/problems/move-zeroes
    
    1190ms 19.2mb

---
+++ leetcode/move-zeroes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        size_t nums_size = nums.size();
+        int last_input = nums_size;
+        
+        for(int i = 0; i < last_input; ++i) {
+            if (nums[i] == 0) {
+                for (int j = i; j < last_input-1; ++j) {
+                    swap(nums[j], nums[j+1]);
+                }
+                nums[nums_size - 1] = 0;
+                --last_input;
+                --i;
+            }
+        }
+    }
+};
