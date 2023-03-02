8b443ab2805c6305737a16f16764604e6e5e717c
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-03-02 21:15:15 +0300

    linked-list-cycle
    https://leetcode.com/problems/linked-list-cycle
    
    47ms 17.8mb

---
+++ leetcode/linked-list-cycle.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+
+        s = set()
+        ptr = head
+
+        while ptr:
+            if ptr in s:
+                return True
+            s.add(ptr)
+            ptr = ptr.next
+
+        return False
