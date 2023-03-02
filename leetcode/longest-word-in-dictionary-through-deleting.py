a4f92b4e03fa833f8c9b5e96870e392a4bc6fe65
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-03-02 10:54:25 +0300

    longest-word-in-dictionary-through-deleting
    https://leetcode.com/problems/longest-word-in-dictionary-through-deleting
    
    307ms 16.6mb

---
+++ leetcode/longest-word-in-dictionary-through-deleting.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findLongestWord(self, s: str, dictionary: List[str]) -> str:
+        dictionary = sorted(dictionary, key=lambda s: (-len(s), s))
+
+        s_length = len(s)
+
+        for d in dictionary:
+            placeholder = 0
+            seeker = 0
+            d_length = len(d)
+
+            if d_length > s_length:
+                continue
+
+            while placeholder < d_length:
+                while seeker < s_length and s[seeker] != d[placeholder]:
+                    seeker += 1
+
+                if seeker == s_length:
+                    break
+
+                placeholder += 1
+                seeker += 1
+
+            if placeholder == d_length:
+                return d
+
+        return ""
