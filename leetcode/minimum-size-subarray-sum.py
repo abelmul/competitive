cc9e0647d1c4d460899ceb085beade8d78703be9
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-03-07 21:05:55 +0300

    minimum-size-subarray-sum
    https://leetcode.com/problems/minimum-size-subarray-sum
    
    253ms 27.2mb

---
+++ leetcode/minimum-size-subarray-sum.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        min_length = 100001  # see constraints
+        length = len(nums)
+
+        l = s = 0
+
+        for i in range(length):
+            s += nums[i]
+
+            if s >= target:
+                while s >= target:
+                    s -= nums[l]
+                    l += 1
+
+                min_length = min(min_length, i - l + 2)
+        if min_length > 100000:
+            min_length = 0
+
+        return min_length
