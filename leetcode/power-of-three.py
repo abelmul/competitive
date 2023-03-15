895abdbb7c72fd9987854a752d256f2ff49acb52
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-03-15 12:13:11 +0300

    power-of-three
    https://leetcode.com/problems/power-of-three
    
    84ms 13.8mb

---
+++ leetcode/power-of-three.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n == 1:
+            return True
+
+        elif n < 1:
+            return False
+
+        return n % 3 == 0 and self.isPowerOfThree(n // 3)
