57497723f4144a4882ae32c72d8dbc58fffcba7e
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-03-15 12:13:12 +0300

    powx-n
    https://leetcode.com/problems/powx-n
    
    20ms 13.9mb

--- leetcode/powx-n.py
+++ leetcode/powx-n.py
@@ -1,3 +1,22 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x**n
+        if n == 0:
+            return 1
+
+        if n == 1 or x == 0 or x == 1:
+            return x
+
+        negative = False
+        if n < 0:
+            negative = True
+            n = abs(n)
+
+        ans = 1
+        if n % 2 == 1:
+            ans = x
+            n -= 1
+
+        half = self.myPow(x, n // 2)
+        ans = ans * half * half
+
+        return 1 / ans if negative else ans
