7d98e89b0dc16492443e5f9888b635903d7e9aeb
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-03-15 12:17:38 +0300

    recursive-digit-sum
    https://www.hackerrank.com/challenges/recursive-digit-sum

---
+++ hackerrank/recursive-digit-sum.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import os
+
+#
+# Complete the 'superDigit' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. STRING n
+#  2. INTEGER k
+#
+
+
+def superDigit(n, k):
+    # Write your code here
+
+    if len(n) == 1:
+        return n
+
+    return superDigit(superDigit(str(sum(map(int, list(n)))), 1) * k, 1)
+
+
+if __name__ == "__main__":
+    fptr = open(os.environ["OUTPUT_PATH"], "w")
+
+    first_multiple_input = input().rstrip().split()
+
+    n = first_multiple_input[0]
+
+    k = int(first_multiple_input[1])
+
+    result = superDigit(n, k)
+
+    fptr.write(str(result) + "\n")
+
+    fptr.close()
