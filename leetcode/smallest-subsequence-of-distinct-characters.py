4a31b7d9d8b1a53d1e96f21322e8050180a5a331
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-03-22 22:47:33 +0300

    smallest-subsequence-of-distinct-characters
    https://leetcode.com/problems/smallest-subsequence-of-distinct-characters
    
    37ms 13.9mb

---
+++ leetcode/smallest-subsequence-of-distinct-characters.py
@@ -0,0 +1,16 @@
+from typing import Counter
+
+
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        st = []  # min stack
+        letters = Counter(s)
+
+        for c in s:
+            while st and c not in st and c < st[-1] and letters[st[-1]] > 0:
+                st.pop()
+
+            letters[c] -= 1
+            if c not in st:
+                st.append(c)
+        return "".join(st)
