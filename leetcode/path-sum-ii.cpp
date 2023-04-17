f9ccb756eb6c82ef1599caf1563273a2d2ad4757
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-04-17 12:25:38 +0300

    path-sum-ii
    https://leetcode.com/problems/path-sum-ii
    
    24ms 31mb

--- leetcode/path-sum-ii.cpp
+++ leetcode/path-sum-ii.cpp
@@ -9,10 +9,7 @@ struct TreeNode {
     TreeNode *right;
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right)
-        : val(x), left(left), right(right)
-    {
-    }
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Solution
@@ -23,30 +20,29 @@ class Solution
         vector<vector<int>> res;
         vector<int> cur;
 
-        function<void(TreeNode *, vector<int> &)> backtrack =
-            [&](TreeNode *node, vector<int> cur) {
-                if (!node || (!node->left && !node->right)) {
-                    // leaf node
-                    if (accumulate(cur.begin(), cur.end(), 0) == targetSum) {
-                        res.push_back(cur);
-                    }
-                    return;
+        function<void(TreeNode *, vector<int> &)> backtrack = [&](TreeNode *node, vector<int> cur) {
+            if (!node || (!node->left && !node->right)) {
+                // leaf node
+                if (accumulate(cur.begin(), cur.end(), 0) == targetSum) {
+                    res.push_back(cur);
                 }
+                return;
+            }
 
-                // leave right
-                if (node->left) {
-                    cur.push_back(node->left->val);
-                    backtrack(node->left, cur);
-                    cur.pop_back();
-                }
+            // leave right
+            if (node->left) {
+                cur.push_back(node->left->val);
+                backtrack(node->left, cur);
+                cur.pop_back();
+            }
 
-                // leave left
-                if (node->right) {
-                    cur.push_back(node->right->val);
-                    backtrack(node->right, cur);
-                    cur.pop_back();
-                }
-            };
+            // leave left
+            if (node->right) {
+                cur.push_back(node->right->val);
+                backtrack(node->right, cur);
+                cur.pop_back();
+            }
+        };
 
         if (root) {
             cur.push_back(root->val);
