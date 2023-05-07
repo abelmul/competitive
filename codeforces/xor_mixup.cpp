9c7bc092f842e2c4de1f5557a36dcfacea41dddf
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-05-07 16:21:35 +0300

    xor-mixup
    https://codeforces.com/contest/1698/problem/A
    
    31ms 0KB

---
+++ codeforces/xor_mixup.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * XOR Mixup
+ * https://codeforces.com/contest/1698/problem/A
+ *
+ * Time - O(n^2)
+ * Space - O(1)
+ */
+
+int main()
+{
+    int t, n, res;
+    vector<int> a;
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> t;
+
+    for (auto _ = 0; _ < t; ++_) {
+        cin >> n;
+
+        a.resize(n);
+        for (auto i = 0; i < n; ++i) cin >> a[i];
+
+        /**
+         * because
+         * a[0] ^ a[1] ^ ...a[n - 1] = 0
+         * implies
+         * a[0] ^ a[1] ^ ...a[n-1] ^ a[n - 1] = a[0] ^ a[1] ^ ...a[n-2] = a[n - 1]
+         */
+        cout << a[0] << "\n";
+    }
+
+    return 0;
+}
