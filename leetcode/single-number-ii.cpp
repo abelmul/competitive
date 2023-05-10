ae497a081bf5270d495d2ad07a699ab24c1a6503
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-05-11 00:37:20 +0300

    single-number-ii
    https://leetcode.com/problems/single-number-ii
    
    7ms 9.5mb

--- leetcode/single-number-ii.cpp
+++ leetcode/single-number-ii.cpp
@@ -1,24 +1,18 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
 class Solution
 {
  public:
     int singleNumber(vector<int>& nums)
     {
         int res = 0;
-        int count[32] = {0};
-
-        for (auto n : nums) {
-            for (auto i = 0; i < 32; ++i) {
-                if (n & (1 << i))
-                    ++count[i];
-            }
-        }
+        int count = 0;
 
         for (auto i = 0; i < 32; ++i) {
-            if (count[i] % 3 == 1) {
+            count = 0;
+            for (auto n : nums) {
+                if (n & (1 << i))
+                    ++count;
+            }
+            if (count % 3 == 1) {
                 res = res | (1 << i);
             }
         }
