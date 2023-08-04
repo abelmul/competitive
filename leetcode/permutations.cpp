b33e65bbd9b4d94f882c73842cfb4efef749d9b8
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-08-04 21:51:49 +0300

    permutations
    https://leetcode.com/problems/permutations
    
    5ms 7.6mb

--- leetcode/permutations.cpp
+++ leetcode/permutations.cpp
@@ -2,19 +2,21 @@
 
 using namespace std;
 
-class Solution {
-public:
-    vector<vector<int>> permute(vector<int>& nums) {
+class Solution
+{
+ public:
+    vector<vector<int>> permute(vector<int>& nums)
+    {
         vector<vector<int>> res;
         int size = nums.size();
-        
+
         function<void(int, vector<int>&)> backtrack = [&](int l, vector<int>& cur) {
             if (l >= size) {
                 res.push_back(cur);
                 return;
             }
 
-            for(auto i = l; i < size; ++i) {
+            for (auto i = l; i < size; ++i) {
                 swap(cur[l], cur[i]);
 
                 backtrack(l + 1, cur);
