25448b6a7d83453273fa423ae135ef928230c4b0
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-08-19 19:45:11 +0300

    maximal-network-rank
    https://leetcode.com/problems/maximal-network-rank
    
    113ms 42.4mb

--- leetcode/maximal-network-rank.cpp
+++ leetcode/maximal-network-rank.cpp
@@ -7,18 +7,20 @@ class Solution
  public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads)
     {
-        vector<set<int>> adjlist(n);
+        unordered_map<int, unordered_set<int>> adjList;
         int res = 0;
 
         for (auto r : roads) {
-            adjlist[r[0]].insert(r[1]);
-            adjlist[r[1]].insert(r[0]);
+            adjList[r[0]].insert(r[1]);
+            adjList[r[1]].insert(r[0]);
         }
 
         for (auto i = 0; i < n; ++i) {
             for (auto j = i + 1; j < n; ++j) {
-                res = max(res, (int)(adjlist[i].size() + adjlist[j].size() -
-                                     (adjlist[i].find(j) != adjlist[i].end())));
+                bool hasDirectConnection = adjList[i].find(j) != adjList[i].end();
+
+                res = max(res, static_cast<int>(adjList[i].size() + adjList[j].size() -
+                                                hasDirectConnection));
             }
         }
 
