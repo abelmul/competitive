5ad42e7f623902c1acb06bed93a9af1459a2cd44
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-11-11 20:39:01 +0300

    valid-palindrome
    https://leetcode.com/problems/valid-palindrome
    
    55ms 19.2mb

--- leetcode/valid-palindrome.py
+++ leetcode/valid-palindrome.py
@@ -1,7 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = "".join([c.lower() for c in s if c.isalnum()])
-        print(s)
 
         begin = 0
         end = len(s) - 1
