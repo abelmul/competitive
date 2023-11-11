693a3ea61706b41fab2e0125d8e398563603a068
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-11-11 20:39:08 +0300

    fibonacci-number
    https://leetcode.com/problems/fibonacci-number
    
    0ms 6mb

--- leetcode/fibonacci-number.cpp
+++ leetcode/fibonacci-number.cpp
@@ -1,14 +1,26 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+
 class Solution
 {
  public:
+    int recurse(int n, vector<int>& memo)
+    {
+        if (n <= 1)
+            return n;
+        if (memo[n] != -1)
+            return memo[n];
+
+        memo[n] = recurse(n - 1, memo) + recurse(n - 2, memo);
+
+        return memo[n];
+    }
+
     int fib(int n)
     {
-        if (n == 0)
-            return 0;
-        if (n == 1)
-            return 1;
-        return fib(n - 1) + fib(n - 2);
+        vector<int> memo(n + 2, -1);
+
+        return recurse(n, memo);
     }
 };
