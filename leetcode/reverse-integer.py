9227da147a0c96313e5714dc584df3680066925f
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-11-11 20:39:00 +0300

    reverse-integer
    https://leetcode.com/problems/reverse-integer
    
    31ms 13.8mb

--- leetcode/reverse-integer.py
+++ leetcode/reverse-integer.py
@@ -1,9 +1,11 @@
 class Solution:
     def reverse(self, x: int) -> int:
         numbers = []
-        negative = False if x >= 0 else True
 
-        x = abs(x)
+        negative = False
+        if x < 0:
+            negative = True
+            x *= -1
 
         while x != 0:
             numbers.append(x % 10)
