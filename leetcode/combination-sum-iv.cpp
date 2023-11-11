21672e8db68b8b682bb9ebe4600300d6b79095a0
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-11-11 20:39:10 +0300

    combination-sum-iv
    https://leetcode.com/problems/combination-sum-iv
    
    5ms 6.8mb

--- leetcode/combination-sum-iv.cpp
+++ leetcode/combination-sum-iv.cpp
@@ -7,26 +7,22 @@ class Solution
  public:
     int combinationSum4(vector<int>& nums, int target)
     {
-        int res = 0;
-
         vector<int> memo(target + 1, -1);
-
         function<int(int)> dp = [&](int amount) {
-            if (amount <= 0) {
-                return static_cast<int>(amount == 0);
+            if (amount >= target) {
+                return static_cast<int>(amount == target);
             }
 
             if (memo[amount] == -1) {
                 memo[amount] = 0;
-
                 for (auto n : nums) {
-                    memo[amount] += dp(amount - n);
+                    memo[amount] += dp(amount + n);
                 }
             }
 
             return memo[amount];
         };
 
-        return dp(target);
+        return dp(0);
     }
 };
