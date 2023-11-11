51c041b9d2875c0f6a503d08515f022f90c37cc8
Abel Mulugeta <abelmulugeta2014@gmail.com>
2023-11-11 20:20:16 +0300

    chore: update leetcode_submissions

--- scripts/leetcode_submissions.py
+++ scripts/leetcode_submissions.py
@@ -6,6 +6,7 @@ import os
 import sys
 from os.path import exists, expanduser
 from subprocess import check_output
+from time import sleep
 
 from httpx import Client
 
@@ -17,8 +18,7 @@ cookies = {
     "NEW_PROBLEMLIST_PAGE": "1",
 }
 
-submitted = {}
-limit = int(os.environ.get("LEETCODE_LIMIT", 100))
+limit = 20
 
 print(f"changing directory to {competitive_folder}")
 os.chdir(competitive_folder)
@@ -31,48 +31,80 @@ client = Client(
 )
 
 params = {"limit": limit}
+extConverter = {
+    "python3": "py",
+    "python": "py",
+    "java": "java",
+    "cpp": "cpp",
+    "csharp": "cs",
+    "javascript": "js",
+    "ruby": "rb",
+    "swift": "swift",
+    "golang": "go",
+    "scala": "sc",
+    "kotlin": "kt",
+    "rust": "rs",
+    "typescript": "ts",
+    "racket": "rkt",
+    "erlang": "erl",
+    "elixir": "ex",
+    "php": "php",
+}
 
-response = client.get("/", params=params, cookies=cookies)
-cookies.update(response.cookies)
-cookies = response.cookies
-res = response.json()
+
+def getSubmissions(last, limit):
+    params = {"offset": last, "limit": limit}
+    return client.get("/", params=params, cookies=cookies)
 
 
-if response.status_code == 200:
-    for submission in res["submissions_dump"]:
-        extension = ""
-        if submission["lang"] == "cpp":
-            extension = "cpp"
-        else:
-            extension = "py"
+def processResults(last, response, submitted):
+    res = response.json()
+    if response.status_code == 200:
+        for submission in res["submissions_dump"]:
+            extension = extConverter.get(submission["lang"], "")
 
-        filename = f'{submission["title_slug"]}.{extension}'
-        if (
-            filename not in submitted
-            and submission["status"] == 10
-            and (
-                not exists(filename)
-                or submission["timestamp"]
-                > int(
-                    check_output(["git", "log", "-n 1", "--format='%at'", filename])
-                    .decode(sys.stdout.encoding)
-                    .strip()
-                    .replace("'", "")
+            filename = f'{submission["title_slug"]}.{extension}'
+            if (
+                filename not in submitted
+                and submission["status"] == 10
+                and (
+                    not exists(filename)
+                    or submission["timestamp"]
+                    > int(
+                        check_output(["git", "log", "-n 1", "--format='%at'", filename])
+                        .decode(sys.stdout.encoding)
+                        .strip()
+                        .replace("'", "")
+                    )
                 )
-            )
-        ):
-            submitted[filename] = submission
+            ):
+                submitted[filename] = submission
 
-for filename, submission in reversed(submitted.items()):
-    message = f'{submission["title_slug"]}\n\nhttps://leetcode.com/problems/{submission["title_slug"]}\n\n{submission["runtime"].replace(" ", "")} {submission["memory"].replace(" ", "").lower()}'
+        last = last + limit
 
-    print(f"writing {submission['title_slug']}")
-    file = open(filename, "w")
-    file.write(submission["code"])
-    file.close()
+        if res["has_next"]:
+            print("Submissions saved so far:  ", last)
+            sleep(2.5)
+            processResults(last, getSubmissions(last, limit), submitted)
 
-    os.system("git reset")
-    if filename.split(".")[-1] == "py":
-        os.system(f"black {filename}")
-    os.system(f"git add {filename}")
-    os.system(f'git commit -m "{message}"')
+        for filename, submission in reversed(submitted.items()):
+            message = f'{submission["title_slug"]}\n\nhttps://leetcode.com/problems/{submission["title_slug"]}\n\n{submission["runtime"].replace(" ", "")} {submission["memory"].replace(" ", "").lower()}'
+
+            print(f"writing {submission['title_slug']}")
+            file = open(filename, "w")
+            file.write(submission["code"])
+            file.close()
+
+            os.system("git reset")
+            if filename.split(".")[-1] == "py":
+                os.system(f"black {filename}")
+
+            os.system(f"git add {filename}")
+            if os.system("git diff --quiet && git diff --cached --quiet") != 0:
+                os.system(f'git commit -m "{message}"')
+
+    else:
+        print("error occurred:", res)
+
+
+processResults(0, getSubmissions(0, limit), {})
